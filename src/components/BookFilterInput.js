f949733f6b57908c06075267187a1b04bbad056c
rosswu24 <wuyawchia24@gmail.com>
2017-08-27 14:52:34 -1000

    added and finished Book.js/BookFilterInput/BookListAppTitle

---
+++ src/components/BookFilterInput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BookFilterInput = ({filterInputChange}) => {
+  // console.log(filterInputChange)
+  return (
+    <div className="book-filter">
+      <label htmlFor="book-filter">Filter Books</label>
+      <br />
+      <input id="book-filter" type="text" onChange={filterInputChange}/>
+    </div>
+  )
+}
+
+export default BookFilterInput;
---
+++ src/components/BookListAppTitle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const BooklistAppTitle = (props) =>{
+  console.log(props);
+  return (
+    <h1>{props.title}</h1>
+    );
+}
+
+export default BooklistAppTitle
